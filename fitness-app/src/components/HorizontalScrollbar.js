948e84ede8f5bd2cb2aa085e77a1af7fa51f50ce
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2022-08-08 00:42:33 +0100

    Add files via upload

---
+++ fitness-app/src/components/HorizontalScrollbar.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { Box, Typography } from "@mui/material";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+
+import BodyPart from "./BodyPart";
+import ExerciseCard from "./ExerciseCard";
+
+const HorizontalScrollbar = ({ data, bodypart, setBodyPart, isBodyParts }) => {
+  return (
+    <ScrollMenu>
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          m="0 40pxs"
+        >
+          {isBodyParts ? (
+            <BodyPart
+              item={item}
+              bodypart={bodypart}
+              setBodyPart={setBodyPart}
+            />
+          ) : (
+            <ExerciseCard exercise={item} />
+          )}
+        </Box>
+      ))}
+    </ScrollMenu>
+  );
+};
+
+export default HorizontalScrollbar;
---
+++ fitness-app/src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    "X-RapidAPI-Key": "0c49535404msh05ce63903f759c2p18f422jsn0ba59dba736b",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
